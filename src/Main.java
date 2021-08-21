1b789c2c58dc5e3e5a0065bb98bcb318634d82f5
dominikamarchewka <87395544+dominikamarchewka@users.noreply.github.com>
2021-08-21 21:43:19 +0200

    Setting up connection

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Main {
 
+    private static HttpURLConnection connection;
+
     public static void main(String[] args) {
+        try {
+            URL url = new URL("https://jsonplaceholder.typicode.com/albums");
+            connection = (HttpURLConnection) url.openConnection();
+
+            //Request setup
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+
+            int status = connection.getResponseCode();
+            System.out.println(status);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
