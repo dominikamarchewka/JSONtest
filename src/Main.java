037e042d44dc9b5ea3b21249a8646ada915e6f1f
dominikamarchewka <87395544+dominikamarchewka@users.noreply.github.com>
2021-08-21 22:14:25 +0200

    Finished first method

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -8,6 +10,11 @@ public class Main {
     private static HttpURLConnection connection;
 
     public static void main(String[] args) {
+        //First method
+        BufferedReader reader;
+        String line;
+        StringBuffer responseContent = new StringBuffer();
+
         try {
             URL url = new URL("https://jsonplaceholder.typicode.com/albums");
             connection = (HttpURLConnection) url.openConnection();
@@ -20,11 +27,26 @@ public class Main {
             int status = connection.getResponseCode();
             System.out.println(status);
 
+            if (status > 299){
+                reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+                while ((line= reader.readLine()) != null){
+                    responseContent.append(line);
+                }
+                reader.close();
+            } else {
+                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                while ((line= reader.readLine()) != null){
+                    responseContent.append(line);
+                }
+                reader.close();
+            }
+            System.out.println(responseContent.toString());
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            connection.disconnect();
         }
-
     }
 }
