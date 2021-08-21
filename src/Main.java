87b0e4d64604fd225868aec86f4ea7f2057d0bdc
dominikamarchewka <87395544+dominikamarchewka@users.noreply.github.com>
2021-08-21 22:32:23 +0200

    Finished second method (HttpClient)

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import java.net.URI;
 import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class Main {
 
@@ -57,7 +58,10 @@ public class Main {
         //Second method java.net.HttpClient
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://jsonplaceholder.typicode.com/albums")).build();
-
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();
 
     }
 }
